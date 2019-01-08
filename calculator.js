16d9a4092011490df2d71928140c9daa20f57a59
solidstatejake <jake.berg@sunydutchess.edu>
2019-01-08 14:46:36 -0500

    Finish project.

--- calculator.js
+++ calculator.js
@@ -1,5 +1,24 @@
 const body = document.querySelector("#calculator-body-container"), numbers_container = document.querySelector("#numbers-container"), operators_container = document.querySelector("#operators-container"),
-equal_clear_decimalpoint = document.querySelector("#equal-clear-container");
+equal_clear_decimalpoint = document.querySelector("#equal-clear-container"),
+calculator_screen = document.querySelector(".screen");
+
+function clearScreen(){
+  calculator_screen.value = "";
+}
+
+function evaluateScreen(){
+  calculator_screen.value = eval(calculator_screen.value);
+}
+
+function factorial(argument){
+  let factorial = 1, count = 0;
+
+  for(let i = 0; i < argument; i++){
+    factorial *= argument - i;
+    console.log(factorial, argument);
+  }
+  return factorial;
+}
 
 // Create number buttons
 for(let i = 1; i <= 10; i++){
@@ -14,7 +33,11 @@ for(let i = 1; i <= 10; i++){
   i === 10 ?  //If 10, replace with 0 and make button larger.
     (button.style.width = "300px", number.textContent = "0") : button.style.width = "100px";
   
-  
+  button.addEventListener("click", ()=>{
+    console.log(number.textContent);
+    calculator_screen.value += `${number.textContent}`;
+  })
+
   button.appendChild(number);
   numbers_container.appendChild(button);
 }
@@ -35,7 +58,7 @@ for(let i = 0; i < 6; i++){
       operator.textContent = "-";
       break;
     case 2:
-      operator.textContent = "x";
+      operator.textContent = "*";
       break;
     case 3:
       operator.textContent = "/";
@@ -47,6 +70,10 @@ for(let i = 0; i < 6; i++){
       operator.textContent = "!";
       break;
   }
+
+  button.addEventListener("click", ()=>{
+    calculator_screen.value += operator.textContent;
+  })
 
   button.appendChild(operator);
   operators_container.appendChild(button);
@@ -62,14 +89,28 @@ for(let i = 0; i < 3; i++){
   function_name.classList.add("equal-clear-decimalpoint-button-content");
 
   switch(i){
+    //"equals" button
     case 0:
       function_name.textContent = "=";
+      button.addEventListener("click", () => {
+        //Test input for factorial; extract and calc factorial if found; ignore rest
+        /^[0-9]+!$/.test(calculator_screen.value) === true ? 
+        calculator_screen.value = factorial(parseInt(calculator_screen.value.match(/^[0-9]+/).join())) : evaluateScreen();
+      });
       break;
+    //clear button
     case 1:
       function_name.textContent = "Clear";
+      button.addEventListener("click", clearScreen);
       break;
+    //decimal point
     case 2:
       function_name.textContent = ".";
+      button.addEventListener("click", ()=>{
+        //test if a decimal point has already been appended to buffer
+        if(/[.]/.test(calculator_screen.value) === false){ 
+          calculator_screen.value += ".";}
+      });
       break;
   }
 
